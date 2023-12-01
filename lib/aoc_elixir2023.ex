57105aa5c5d49f18c4a944ddafdfd393322b6011
Daniel Keefer <daniel.keefer@transmax.com.au>
2023-12-01 19:34:55 +1000

    feat: :tada: start aoc 2023

---
+++ lib/aoc_elixir2023.ex
@@ -0,0 +1,18 @@
+defmodule AocElixir2023 do
+  @moduledoc """
+  Documentation for `AocElixir2023`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AocElixir2023.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc_elixir2023_test.exs
@@ -0,0 +1,8 @@
+defmodule AocElixir2023Test do
+  use ExUnit.Case
+  doctest AocElixir2023
+
+  test "greets the world" do
+    assert AocElixir2023.hello() == :world
+  end
+end
